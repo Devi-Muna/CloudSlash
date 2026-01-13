b9bc8929085b47213e71163704aa6cd9ca8dd28d
DrSkyle <drskyle8000@gmail.com>
2026-01-13 17:49:13 +0545

    Enhanced region picker with full global AWS region list (28+) and implemented scrollable viewport.

--- cmd/cloudslash/commands/picker.go
+++ cmd/cloudslash/commands/picker.go
@@ -17,10 +17,17 @@ type regionModel struct {
 func initialRegionModel() regionModel {
 	return regionModel{
 		choices: []string{
+			// US
 			"us-east-1", "us-east-2", "us-west-1", "us-west-2",
-			"ca-central-1", "sa-east-1",
-			"eu-central-1", "eu-west-1", "eu-west-2", "eu-west-3", "eu-north-1",
-			"ap-south-1", "ap-southeast-1", "ap-southeast-2", "ap-northeast-1", "ap-northeast-2",
+			// Europe
+			"eu-central-1", "eu-central-2", "eu-west-1", "eu-west-2", "eu-west-3", "eu-north-1", "eu-south-1", "eu-south-2",
+			// APAC
+			"ap-south-1", "ap-south-2", "ap-southeast-1", "ap-southeast-2", "ap-southeast-3", "ap-southeast-4",
+			"ap-northeast-1", "ap-northeast-2", "ap-northeast-3", "ap-east-1",
+			// Americas
+			"ca-central-1", "ca-west-1", "sa-east-1",
+			// Africa / Middle East
+			"af-south-1", "me-south-1", "me-central-1", "il-central-1",
 		},
 		selected: make(map[int]struct{}),
 	}
@@ -39,10 +46,16 @@ func (m regionModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		case "up", "k":
 			if m.cursor > 0 {
 				m.cursor--
+			} else {
+				// Loop to bottom
+				m.cursor = len(m.choices) - 1
 			}
 		case "down", "j":
 			if m.cursor < len(m.choices)-1 {
 				m.cursor++
+			} else {
+				// Loop to top
+				m.cursor = 0
 			}
 		case " ", "x":
 			_, ok := m.selected[m.cursor]
@@ -63,7 +76,27 @@ func (m regionModel) View() string {
 	s.WriteString(lipgloss.NewStyle().Bold(true).Foreground(lipgloss.Color("205")).Render("? Which regions do you want to scan?"))
 	s.WriteString("\n\n")
 
-	for i, choice := range m.choices {
+	// Paginator Logic
+	// Window size = 10
+	windowHeight := 10
+	start := 0
+	end := len(m.choices)
+
+	if len(m.choices) > windowHeight {
+		if m.cursor < windowHeight/2 {
+			start = 0
+			end = windowHeight
+		} else if m.cursor >= len(m.choices)-windowHeight/2 {
+			start = len(m.choices) - windowHeight
+			end = len(m.choices)
+		} else {
+			start = m.cursor - windowHeight/2
+			end = m.cursor + windowHeight/2
+		}
+	}
+
+	for i := start; i < end; i++ {
+		choice := m.choices[i]
 		cursor := " "
 		if m.cursor == i {
 			cursor = ">"
@@ -74,7 +107,17 @@ func (m regionModel) View() string {
 			checked = "x"
 		}
 
-		s.WriteString(fmt.Sprintf("%s [%s] %s\n", cursor, checked, choice))
+		style := lipgloss.NewStyle()
+		if m.cursor == i {
+			style = style.Foreground(lipgloss.Color("205")).Bold(true)
+		}
+
+		line := fmt.Sprintf("%s [%s] %s", cursor, checked, choice)
+		s.WriteString(style.Render(line) + "\n")
+	}
+	
+	if len(m.choices) > windowHeight {
+		s.WriteString(lipgloss.NewStyle().Foreground(lipgloss.Color("240")).Render(fmt.Sprintf("\n... %d more regions (use arrow keys) ...\n", len(m.choices)-end)))
 	}
 
 	s.WriteString("\n(Press [space] to select, [enter] to confirm)\n")
